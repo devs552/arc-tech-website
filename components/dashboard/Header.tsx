81a7df1719ff875f22f82d00c481d0ab8e5b81de
devs552 <abdullah@gmail.com>
2026-05-22 03:42:48 +0500

    other way to fix  issue of redirect

--- components/dashboard/Header.tsx
+++ components/dashboard/Header.tsx
@@ -265,7 +265,10 @@ function DropdownMenu({ name, email, role, onClose }: DropdownProps) {
       <div style={{ borderTop: `1px solid ${T.dropDivider}`, padding: '6px 6px' }}>
         <button
           type="button"
-          onClick={() => signOut({ redirectTo: '/dashboard' })}
+         onClick={async () => {
+  await signOut({ redirect: false });
+  (window.location.href = '/dashboard');
+}}
           onMouseEnter={() => setHovered('signout')}
           onMouseLeave={() => setHovered(null)}
           style={{
